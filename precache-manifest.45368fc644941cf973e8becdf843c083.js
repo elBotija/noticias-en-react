6fecfd1983f8053fd810415076c8c791409e6ddc
mmartinez <mmartinez@dosalcubo.com>
2019-01-16 11:36:18 -0300

    Updates

---
+++ precache-manifest.45368fc644941cf973e8becdf843c083.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf46c7eee52885e28a70",
+    "url": "/noticias-en-react/static/js/runtime~main.cf46c7ee.js"
+  },
+  {
+    "revision": "8a946222008a7d8d8c26",
+    "url": "/noticias-en-react/static/js/main.8a946222.chunk.js"
+  },
+  {
+    "revision": "036b432e89d30ce0b437",
+    "url": "/noticias-en-react/static/js/1.036b432e.chunk.js"
+  },
+  {
+    "revision": "8a946222008a7d8d8c26",
+    "url": "/noticias-en-react/static/css/main.c41002f4.chunk.css"
+  },
+  {
+    "revision": "3562a08b1e79ddb3ecfe78b1a520ae88",
+    "url": "/noticias-en-react/index.html"
+  }
+];
